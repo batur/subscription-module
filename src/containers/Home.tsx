32c7b33e1c836c4c3db178cb0d31cf10c33f6810
Batur Akkurt <halidbaturakkurt@gmail.com>
2021-11-09 00:42:33 +0300

    Add Layout wrapper - Add react-router, react-feather - Remove react-app-rewired - Update public assets - Add Header & Main - Add 404 Page

---
+++ src/containers/Home.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <section className="h-100 d-flex justify-content-center align-items-center">
+        <h1>Home</h1>
+      </section>
+    );
+  }
+}
+
+export default Home;
---
+++ src/containers/NotFound.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <section className="h-100 d-flex justify-content-center align-items-center">
+        <h1>404 Not Found</h1>
+      </section>
+    );
+  }
+}
+
+export default NotFound;
